ca4be81e3c1f4483faf04d18654a183c9e1c4d26
Jorge Alfredo <jorge.alvarez7402@alumnos.udg.mx>
2023-09-22 20:51:01 -0600

    empezado ejercicio 2 de la tarea 2

---
+++ tareas-aned/tarea2_ej2_jaac_aned_main.py
@@ -0,0 +1,20 @@
+import random as rd
+import matplotlib.pyplot as plt
+
+ws = []
+N = 10000
+for n in range(1, N):
+    z = complex(6 * (rd.random() - 0.5), 6 * (rd.random() - 0.5))
+    w = 1 + z + z**2 / 2
+    if (abs(w) < 1):
+        ws.append(z)
+
+
+def lmap(f, lst):
+    return [f(x) for x in lst]
+
+
+plt.scatter(lmap(lambda x: x.real, ws), lmap(lambda x: x.imag, ws))
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.show()
