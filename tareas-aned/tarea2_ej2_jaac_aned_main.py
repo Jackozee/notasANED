a2251f67adc2e0f12458b8157a972db282a64abc
Jackozee <narutouzu_pop@hotmail.com>
2023-09-25 06:39:13 -0600

    tarea 2 solo falta ej5

--- tareas-aned/tarea2_ej2_jaac_aned_main.py
+++ tareas-aned/tarea2_ej2_jaac_aned_main.py
@@ -1,12 +1,25 @@
 import random as rd
+import numpy as np
 import matplotlib.pyplot as plt
 
 ws = []
-N = 10000
-for n in range(1, N):
-    z = complex(6 * (rd.random() - 0.5), 6 * (rd.random() - 0.5))
+N = 15000
+
+
+def cond1(z):
     w = 1 + z + z**2 / 2
-    if (abs(w) < 1):
+    return (abs(w) < 1)
+
+
+def cond2(z):
+    r0 = (2 + np.sqrt(1 + 2 * z)) / (3 - 2 * z)
+    r1 = (2 - np.sqrt(1 + 2 * z)) / (3 - 2 * z)
+    return (abs(r0) < 1 and abs(r1) < 1)
+
+
+for n in range(1, N):
+    z = complex(8 * (rd.random() - 0.5), 8 * (rd.random() - 0.5))
+    if cond2(z):
         ws.append(z)
 
 
@@ -14,9 +27,10 @@ def lmap(f, lst):
     return [f(x) for x in lst]
 
 
-plt.title("Región del plano complejo con |1+z+z²/2|<1")
+# plt.title("Región del plano complejo con |1+z+z²/2|<1")
+plt.title("Región del plano complejo con |r0(z)|<1 y |r1(z)|<1")
 plt.scatter(lmap(lambda x: x.real, ws), lmap(lambda x: x.imag, ws))
-plt.xlim([-3, 3])
+plt.xlim([-4, 4])
 plt.ylim([-3, 3])
 plt.xlabel("Re(z)")
 plt.ylabel("Im(z)")
