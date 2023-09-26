fc8ca5d4e50878721d91b7c3bf6a3e61903a77c7
Jorge Alfredo <jorge.alvarez7402@alumnos.udg.mx>
2023-09-26 16:40:13 -0600

    empezado corr+pred

--- tareas-aned/predictor_corrector.py
+++ tareas-aned/predictor_corrector.py
@@ -1,9 +1,31 @@
+# import numpy as np
+
 
 def predcor(coefcorra, coefcorrb, coefpreda, coefpredb,
-            h, f, t0, u0, tf, pece, m):
-    ordencorr = max(len(coefcorra), len(coefcorrb) - 1)
-    ordenpred = max(len(coefpreda), len(coefpredb) - 1)
-    ordencopr = max(ordencorr, ordenpred)
-    if (len(u0) < ordencopr):
+            h, f, t0, tf, condini, iteracionescorr):
+    T = tf - t0
+    N = round(T / h)
+    pasoscorr = max(len(coefcorra), len(coefcorrb) - 1)
+    pasospred = max(len(coefpreda), len(coefpredb) - 1)
+    pasoscorpr = max(pasoscorr, pasospred)
+    if (len(condini) < pasoscorpr):
         return [], []
-    t = [t0 + n * h for n in range(0, ordencopr - 1)]
+    us = condini[0:pasoscorpr]
+    # y = condini[0:pasoscorpr]
+    ts = [t0 + n * h for n in range(0, N + 1)]
+    fs = [f(ts[i], us[i]) for i in range(0, pasoscorpr)]
+    print(fs)
+
+    # k = pasoscorpr
+    # for t in [t0 + n * h for n in range(pasoscorpr, N + 1)]:
+    # ut = np.dot(coefpreda, u[k:k - pasospred + 1:-1])
+    return us
+
+
+def f(t, y):
+    return y
+
+
+us = predcor([1], [1 / 2, 1 / 2], [1], [1], 0.1, f, 0, 1, [1], 1)
+
+print(us)
